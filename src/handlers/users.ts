21dfa8fe4ed084fca8898e4b04778983a2e30588
daniel-tickell <86997827+daniel-tickell@users.noreply.github.com>
2025-06-20 12:11:11 -0700

    Update users.ts

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -2,35 +2,28 @@ import express, { Request, Response } from 'express'
 import { User, UserStore} from '../models/users.js'
 
 import jwt from 'jsonwebtoken'
-
-
-
 const user = new UserStore()
 
+const verifyAuthToken(auth) = async (req: Request, res: Response, next) => {
+    try {
+        const authorizationHeader = auth;
+        const token = authorizationHeader.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+        next()
+    } catch (err) {
+        res.status(401)
+	res.json(`Invalid Token Recieved: ${err}`);
+    }
+}
+
 const index = async (req: Request, res: Response) => {
-  try {
-	const authorizationHeader = req.headers.authorization
-        const token = authorizationHeader.split(' ')[1]
-	jwt.verify(token, process.env.TOKEN_SECRET);
-  } catch(err) {
-	  res.status(401);
-	  res.json(`Invalid Token Recieved: ${err}`);
-	  return
-  }
+	verifyAuthToken(req.headers.authorization);
 	const getUsers = await user.index();
   	res.json(getUsers);
 }
 
 const show = async (req: Request, res: Response) => {
-  try {
-	const authorizationHeader = req.headers.authorization
-        const token = authorizationHeader.split(' ')[1]
-	jwt.verify(token, process.env.TOKEN_SECRET);  
-} catch(err) {
-	  res.status(401);
-	  res.json(`Invalid Token Recieved: ${err}`);
-	  return
-  }
+	verifyAuthToken(req.headers.authorization); 
 	const showUsers = await user.show(parseInt(req.params.id))
   	res.json(showUsers);
 }
