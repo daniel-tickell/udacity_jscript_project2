277fcf91c619fe09ada21e62f146d658e9c577c8
daniel-tickell <dtickell@gmail.com>
2025-06-21 09:21:02 -0700

    remove build and lock files

--- build/models/order_items.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=order_items.js.map
--- build/models/tests/helpers/reporter.js
+++
@@ -1,9 +0,0 @@
-import { DisplayProcessor, SpecReporter, } from "jasmine-spec-reporter";
-class CustomProcessor extends DisplayProcessor {
-    displayJasmineStarted(info, log) {
-        return `${log}`;
-    }
-}
-jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new SpecReporter());
-//# sourceMappingURL=reporter.js.map
